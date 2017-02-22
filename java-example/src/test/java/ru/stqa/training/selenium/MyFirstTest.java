ae0ee4fffc4e2b2f570cec5bc44799e7331ffa37
Vladimir Isaev <isaevvv@yandex.ru>
2017-02-23 03:10:37 +0400

    Less02. Task01

---
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,43 @@
+package ru.stqa.training.selenium;
+
+/**
+ * Created by Vladimir on 23.02.2017.
+ */
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
